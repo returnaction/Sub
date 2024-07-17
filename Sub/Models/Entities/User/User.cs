7a0d0fa11279dafcf5786a85040659061dc1ee02
returnaction <obergannv@gmail.com>
2024-07-17 14:23:51 -0400

    Add User class

--- Sub/Models/Entities/User/User.cs
+++ Sub/Models/Entities/User/User.cs
@@ -1,6 +1,21 @@
-﻿namespace Sub.Models.Entities.User
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Sub.Models.Entities.User
 {
-    public class User
+    public class User : IdentityUser
     {
+        public bool IsActive { get; set; } = true;
+        public bool IsSuperuser { get; set; } = false;
+        public bool IsVerified { get; set; } = false;
+
+        public string? FirstName { get; set; } = null!;
+        public string? LastName { get; set; } = null!;
+        public string? MiddleName { get; set; } = null!;
+
+        public DateTime? DateOfBirth { get; set; } = null!;
+
+        // picture
+
+
     }
 }
