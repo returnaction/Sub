9e5d4c1a2e36501e0d02b544932e99bdae3e3133
returnaction <obergannv@gmail.com>
2024-07-17 14:02:11 -0400

    Add project files.

---
+++ Sub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
