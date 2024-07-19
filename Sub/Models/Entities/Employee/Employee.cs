54e00f33c6e4c820c072d191abaa2146d397de76
returnaction <obergannv@gmail.com>
2024-07-19 15:22:57 -0400

    add Employee class

--- Sub/Models/Entities/Employee/Employee.cs
+++ Sub/Models/Entities/Employee/Employee.cs
@@ -1,6 +1,29 @@
-﻿namespace Sub.Models.Entities.Employee
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sub.Models.Entities.Employee
 {
     public class Employee
     {
+        [Key]
+        public Guid Id { get; set; }
+        public string Position { get; set; } = null!;
+        public string? Obligation { get; set; } = null!;
+        public string CreatedAt { get; set; } = DateTime.Now.ToString("d");
+        public string? UpdatedAt { get; set; }
+
+        // nav props
+
+        [Required]
+        public Guid UserId { get; set; }
+        [ForeignKey("UserId")]
+        public Sub.Models.Entities.User.User.User User { get; set; } = null!;
+
+        [Required]
+        public Guid CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
+        public Sub.Models.Entities.Company.Company Company { get; set; } = null!;
+
     }
 }
